d06ce7d12c1959ae3e40b6b367e4ff5e6e7f239d
Botegaa <botega123@hotmail.com>
2024-08-17 19:59:27 -0300

    Firebase implement

--- MVVMFirstProject/SceneDelegate.swift
+++ MVVMFirstProject/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let vc = HomeVC()
-        //let navVC = UINavigationController(rootViewController: vc)
+        let vc = LoginVC()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
